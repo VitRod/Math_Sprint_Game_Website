227d18992941dd4ae1856ded8963f29b933d8b15
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-05 07:02:00 +0300

    // Pick a remaining element...       randomIndex = Math.floor(Math.random() * currentIndex);       currentIndex -= 1;

--- shuffle.js
+++ shuffle.js
@@ -3,3 +3,7 @@ function shuffle(array) {
 
    // While there remain elements to shuffle...
    while (0 !== currentIndex) { 
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
