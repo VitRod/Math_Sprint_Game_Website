87b11177fb0b3e4b7e209240d1dc433942450d17
Vitaliy Rodyna <ukrdao@gmail.com>
2022-10-05 07:59:52 +0300

    // Pages const gamePage = document.getElementById('game-page'); const scorePage = document.getElementById('score-page'); const splashPage = document.getElementById('splash-page'); const countdownPage = document.getElementById('countdown-page');

---
+++ script.js
@@ -0,0 +1,5 @@
+// Pages
+const gamePage = document.getElementById('game-page');
+const scorePage = document.getElementById('score-page');
+const splashPage = document.getElementById('splash-page');
+const countdownPage = document.getElementById('countdown-page');
